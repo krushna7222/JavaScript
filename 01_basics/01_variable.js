d5e7c7400c85eacc29a4516ffa3c559286b0ad39
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2024-10-22 00:04:47 +0530

    variable and way of declaration

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+const a=20;
+let b= 30;
+var c=40;
+var myname="Krushna Shahane"
+var myname="krushna Shahane Patil"//Name Is Changable wioth the var keyword 
+
+console.log("The Value Of A Is :"+a);
+console.log("The Value Of B Is :"+b);
+console.log("The Value Of C Is :"+c);
+console.log("My Name Is :"+myname);
+
+
+
+//const a=50 // the const a is  declaired first this is not changable with const keyword this is drawback of const keyword
+
+//let b=40; // the let b is already declaided first  this is not changable in java script 
+
+var c=50; // the var c is already declaided first but this is changable in java script
+console.log(c);
+
+console.table([a,b,c]) //using the table we are printing the outpun in table format
