f9168f5cb00e2bdb07ad195962c5375cce077dea
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2024-11-14 09:32:25 +0530

    Date And Its display time and its operations in JavaScript

--- 01_basics/07_dateinJS.js
+++ 01_basics/07_dateinJS.js
@@ -19,12 +19,25 @@ let myTimeStamp = Date.now();
 // console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date();
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
 
 // `${newDate.getDay()} and the time `
 
 newDate.toLocaleString("default", {
   weekday: "long",
 });
+
+// var date = new Date();
+
+// console.log(date.toString());
+
+let date = new Date();
+
+// console.log(date.getMonth() + 1);
+
+let newdate = new Date("2024-12-31");
+
+console.log(newdate.toString());
+console.log(newdate.getTime()); //the give in milisecond
