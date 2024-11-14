e395f7c5988a1171832dc5bc431fccbae9e42040
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2024-11-14 09:08:56 +0530

    Date and its display types and its operations in JavaScript

--- 01_basics/07_dateinJS.js
+++ 01_basics/07_dateinJS.js
@@ -1,34 +1,30 @@
-const score = 400;
-// console.log(score);
+// Dates
 
-const balance = new Number(100);
-// console.log(balance);
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023");
+// console.log(myCreatedDate.toLocaleString());
 
-const otherNumber = 123.8966;
+let myTimeStamp = Date.now();
 
-// console.log(otherNumber.toPrecision(4));
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
 
-const hundreds = 1000000;
-// console.log(hundreds.toLocaleString('en-IN'));
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
 
-// +++++++++++++ Maths +++++++++++++++++++++++++++++
+// `${newDate.getDay()} and the time `
 
-// console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
-// console.log(Math.min(4, 3, 6, 8));
-// console.log(Math.max(4, 3, 6, 8));
-
-console.log(Math.random());
-console.log(Math.random() * 10 + 1);
-console.log(Math.floor(Math.random() * 10) + 1);
-
-const min = 10;
-const max = 20;
-
-console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+newDate.toLocaleString("default", {
+  weekday: "long",
+});
