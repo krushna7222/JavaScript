439d6f5cfd2a3a6576e0e2c9d4c065490a60ecd3
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2024-11-14 19:16:58 +0530

    String and its function in JavaScript

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,35 +1,37 @@
-const name = "krushna";
-const age = 50;
+let str = new String(" Hellow - Krushna ");
+let str1 = new String("How are you");
 
-// console.log(name + age + " Value");
+console.log(str);
 
-console.log(`Hello my name is ${name} and my age is ${age}`);
+let str2 = str.concat(str1); //cincat the two string join the two string
+console.log(str2);
 
-const gameName = new String("krushna-laxman-shahane");
+console.log(`${str} what about you ${str1} `); // join the two string using template literals
 
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
+console.log(str.toLowerCase()); // convert the string to lower case
+console.log(str.toUpperCase()); //convert the styring into upper case
 
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf("t"));
+console.log(str.length); //measure the length of string
 
-const newString = gameName.substring(0, 4);
-console.log(newString);
+console.log(str.trim()); //with the trim function remove the start and end whitespace of the string
 
-const anotherString = gameName.slice(-8, 4);
-console.log(anotherString);
+console.log(str1.charAt(2)); //disply the character is positioned at index 2 of the string index no start with 0
 
-const newStringOne = "   krushna    ";
-console.log(newStringOne);
-console.log(newStringOne.trim()); //trim is remove white spaces
+console.log(str.replace("Krushna", "Hemraj")); //replace the character in the string with the other character
 
-const url = "https://krushna.com/krushna%20shahane";
+let splitedstring = str.split(""); //saparete each character of the string and return the array of character
+console.log(splitedstring);
 
-console.log(url.replace("%20", "-")); //%20 replace with -
+let reversedstring = splitedstring.reverse(); // reverse the string and return the array of character but first split operation required
+console.log(reversedstring);
 
-console.log(url.includes("krushna"));
-console.log(url.includes("hemraj"));
+let joinedstring = reversedstring.join(""); //join the string by reverse order but first split and reverse operation required
+console.log(joinedstring);
 
-console.log(gameName.split("-"));
+console.log(str1.indexOf("are")); //giv the index no of the character in the string if not found return -1
+
+console.log(str1.search("are"));
+
+document.write(str.bold()); //bold the string but in browser web page not in consol
+document.write(str.sub()); //subscript string but in browser web page not in consol
+document.write(str.sup()); //superscript string but in browser web page not in consol
