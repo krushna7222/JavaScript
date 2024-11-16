37fa6dc6131bcdeec699283dbe3bbb60e996146a
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2024-11-17 00:21:47 +0530

    Practice the string and its functions

---
+++ practice/01_string.js
@@ -0,0 +1,44 @@
+let str = new String("Hey Hellow Krishna");
+let str1 = new String(" I Am Fine ");
+
+// Finding Length oif the string
+console.log(str.length);
+console.log(str1.length);
+
+// Joion The String By Concat function
+console.log(str.concat(str1));
+
+// Print The String character with 4 and end with 10
+console.log(str.slice(4, 10));
+
+// split the string in saparate character
+let splitstr = str.split("");
+console.log(splitstr);
+
+// Reverse the split string
+let reversestr = splitstr.reverse();
+console.log(reversestr);
+
+// Join the reversed string then see reversed string
+console.log(reversestr.join(""));
+
+// Fing the index no of character if dose not exist this character then output is -1
+console.log(str.indexOf("K"));
+
+// By trim function remove the start and end whitespaces
+console.log(str1.trim());
+
+// By repeat function string repeat by given number
+console.log(str1.repeat(5));
+
+// By CharAt function we find the character index no by given no
+console.log(str.charAt("15"));
+
+// change the upper case to lower case and lower cxase to upper case
+console.log(str.toUpperCase());
+console.log(str1.toLowerCase());
+
+// Search the character by given index no if yes they output true else they output false
+console.log(str.includes("K", 11));
+
+console.log(str.normalize());
