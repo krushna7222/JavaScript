4ea07ebdbc82dc02ca51106eb810ad79d6709baf
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2024-11-18 22:38:36 +0530

    Type Conversion in JavaScript

---
+++ practice/02_conversion.js
@@ -0,0 +1,11 @@
+var num = 100;
+console.log(typeof num);
+
+let numintostring = String(num);
+console.log(typeof numintostring);
+
+let str = "Krushna";
+console.log(typeof str);
+
+let strintonum = Number(str);
+console.log(typeof strintonum);
