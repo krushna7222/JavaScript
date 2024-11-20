0ba268a4b160952db58f46d6ae6dc547af764053
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2024-11-20 21:38:41 +0530

    Switch Case in JavaScript

---
+++ 02_control_flow/02_switch.js
@@ -0,0 +1,60 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march";
+
+switch (month) {
+  case "jan":
+    console.log("1st month");
+    break;
+  case "feb":
+    console.log("2nd month");
+    break;
+  case "march":
+    console.log("3rd month");
+    break;
+  case "april":
+    console.log("4th month");
+    break;
+
+  default:
+    console.log("default case match");
+    break;
+}
+
+let monthno = 5;
+switch (monthno) {
+  case 1:
+    console.log("January");
+    break;
+
+  case 2:
+    console.log("February");
+    break;
+
+  case 3:
+    console.log("March");
+    break;
+
+  case 4:
+    console.log("April");
+    break;
+
+  case 5:
+    console.log("May");
+    break;
+
+  case 6:
+    console.log("June");
+    break;
+
+  default:
+    console.log("Invalid month number");
+    break;
+}
