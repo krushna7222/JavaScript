406bf27b1534aaa10c4c79eaafbacc10c7b73834
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2024-12-28 10:06:34 +0530

    JavaScript Array and its function and methods

--- practice/03_array.js
+++ practice/03_array.js
@@ -6,19 +6,28 @@
 // console.log(arr.splice(0, 6));
 // console.log(arr);
 
+// let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+// var larr = arr.length;
+
+// console.log("Original Array: ", arr);
+// console.log("Length of the array: ", larr);
+
+// var slicearr = arr.slice(3, 7);
+
+// console.log("Sliced Array: ", slicearr);
+
+// var splicearr = arr.splice(3, 3, 4, 5, 6);
+
+// console.log("Spliced Array: ", splicearr);
+
+// console.log("Updated Array: ", arr);
+
 let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
-var larr = arr.length;
+for (let i in arr) {
+  arr[i] = arr[i] + 10;
+  console.log(arr[i]);
+}
 
-console.log("Original Array: ", arr);
-console.log("Length of the array: ", larr);
-
-var slicearr = arr.slice(3, 7);
-
-console.log("Sliced Array: ", slicearr);
-
-var splicearr = arr.splice(3, 3, 4, 5, 6);
-
-console.log("Spliced Array: ", splicearr);
-
-console.log("Updated Array: ", arr);
+// console.log(arr);
