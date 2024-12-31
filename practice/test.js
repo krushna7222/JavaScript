c8d5294f4eb6d994be013455cf15da0728be765b
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2024-12-31 10:52:49 +0530

    test file for take user info and create object it

---
+++ practice/test.js
@@ -0,0 +1,14 @@
+function userData() {
+  let name = document.getElementsByName("name").values;
+
+  let email = document.getElementsByName("email").values;
+
+  let password = document.getElementsByName("password").values;
+
+  let userInfo = {
+    name: name,
+    email: email,
+    password: password,
+  };
+}
+console.log(userInfo);
