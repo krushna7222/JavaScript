182d6f172ee242e1b4b4a2ffc03afb606bf170f9
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-01-12 22:37:13 +0530

    array file updated

--- practice/03_array.js
+++ practice/03_array.js
@@ -1,29 +1,29 @@
-// let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-// console.log(arr);
+let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(arr);
 
-// console.log(arr.length);
-// console.log(arr.slice(0, 4));
-// console.log(arr.splice(0, 6));
-// console.log(arr);
+console.log(arr.length);
+console.log(arr.slice(0, 4));
+console.log(arr.splice(0, 6));
+console.log(arr);
 
 // let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
-// var larr = arr.length;
+var larr = arr.length;
 
-// console.log("Original Array: ", arr);
-// console.log("Length of the array: ", larr);
+console.log("Original Array: ", arr);
+console.log("Length of the array: ", larr);
 
-// var slicearr = arr.slice(3, 7);
+var slicearr = arr.slice(3, 7);
 
-// console.log("Sliced Array: ", slicearr);
+console.log("Sliced Array: ", slicearr);
 
-// var splicearr = arr.splice(3, 3, 4, 5, 6);
+var splicearr = arr.splice(3, 3, 4, 5, 6);
 
-// console.log("Spliced Array: ", splicearr);
+console.log("Spliced Array: ", splicearr);
 
-// console.log("Updated Array: ", arr);
+console.log("Updated Array: ", arr);
 
-let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
 for (let i in arr) {
   arr[i] += 10;
