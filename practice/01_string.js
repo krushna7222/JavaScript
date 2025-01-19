9a6e3dcf9b9595fe8cf610a492ad17c6afb6751b
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-01-19 21:58:24 +0530

    Strings

--- practice/01_string.js
+++ practice/01_string.js
@@ -48,9 +48,9 @@ console.log(userInputName.toLocaleLowerCase());
 
 // Check The String is Palindrome or not
 
-// let userInput = prompt("Enter the string to check its palindrome or not :");
-// let reverceString = userInput.split("").reverse();
-// let resultString = reverceString.join("");
+let userInput = prompt("Enter the string to check its palindrome or not :");
+let reverceString = userInput.split("").reverse();
+let resultString = reverceString.join("");
 
 if (userInput === resultString) {
   alert("This String is a palindrome");
