fa06578241b529cafb661e6a2fee809b1b73402d
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-01-25 21:55:51 +0530

    Type Conversion In JavaScript

--- practice/02_conversion.js
+++ practice/02_conversion.js
@@ -1,22 +1,22 @@
-// var num = 100;
-// console.log(typeof num);
+var num = 100;
+console.log(typeof num);
 
-// let numintostring = String(num);
-// console.log(typeof numintostring);
+let numintostring = String(num);
+console.log(typeof numintostring);
 
-// let str = "Krushna";
-// console.log(typeof str);
+let str = "Krushna";
+console.log(typeof str);
 
-// let strintonum = Number(str);
-// console.log(typeof strintonum);
+let strintonum = Number(str);
+console.log(typeof strintonum);
 
-// var a = 2;
-// var b = 3;
-// console.log(++a + b++);
+var a = 2;
+var b = 3;
+console.log(++a + b++);
 
-// var z = 10;
-// var y = 20;
+var z = 10;
+var y = 20;
 
-// console.log(z);
-// console.log(++z);
-// console.log(++z + y++);
+console.log(z);
+console.log(++z);
+console.log(++z + y++);
