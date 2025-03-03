5ee2b85d0b41943fe7ba7b6b87b3de5ab92d4f33
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-03-03 23:07:43 +0530

    Arrow Functions in JavaScript

---
+++ 04_functions/03_arrow.js
@@ -0,0 +1,50 @@
+const user = {
+  username: "Krushna",
+  price: 999,
+
+  welcomeMessage: function () {
+    console.log(`${this.username} , welcome to website`);
+    console.log(this);
+  },
+};
+
+// user.welcomeMessage()
+// user.username = "Hemraj"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "Krushna"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "Krushna"
+//     console.log(this.username);
+// }
+
+const chai = () => {
+  let username = "Krushna";
+  console.log(this);
+};
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({ username: "Krushna" });
+
+console.log(addTwo(3, 4));
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
